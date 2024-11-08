7755abd66419e0cd29c345ca57377ef3d5488c4c
Marti820 <j.vasquezc@uniandes.edu.co>
2024-11-08 12:32:07 -0500

    health

--- manejador_usuarios/urls.py
+++ manejador_usuarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PagoListView
+from manejador_usuarios import views
 
 urlpatterns = [
     path("pagos/", PagoListView.as_view(), name='pagos-list'),
+    path('health/', views.health_check, name='health'),
 ]
--- manejador_usuarios/views.py
+++ manejador_usuarios/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Pago
 from .serializers import PagoSerializer
+
+
+from django.http import JsonResponse
+from django.shortcuts import render
+
+def health_check(request):
+    return JsonResponse({'message': 'OK'}, status=200)
 
 class PagoListView(APIView):
     def get(self, request):
